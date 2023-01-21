ef0ee75b4e1e7b9a21208ec16f3593b1613ffa80
Ilya Bakhtin <ilya.bakhtin@gmail.com>
2023-01-21 16:20:49 +0100

    prepare_avs - template.render in s8-avs mode

--- prepare_avs/s8-avs-creator.cpp
+++ prepare_avs/s8-avs-creator.cpp
@@ -9,7 +9,7 @@ S8AvsCreator::S8AvsCreator(const tstring& dir, std::istream& entries_file):
 {
 }
 
-bool S8AvsCreator::process_entry(const std::string& name, const std::string& render_file, const std::string& ns)
+bool S8AvsCreator::process_entry(const std::string& name, const std::string& render_template, const std::string& render_file, const std::string& ns)
 {
 //    template_name_(string_utils::to_ansi_string(template_name.c_str(), CP_UTF8)),
     std::ifstream tmpl(string_utils::to_tstring(name.c_str(), CP_UTF8));
@@ -35,8 +35,9 @@ bool S8AvsCreator::process_entry(const std::string& name, const std::string& ren
 
     const size_t p = name.rfind(".");
     std::string ext;
+
     if (p != std::string::npos)
-        ext = name.substr(p).c_str();
+        ext = name.substr(p);
 
     const bool is_vdscript = ext == ".vdscript";
 
@@ -62,6 +63,9 @@ bool S8AvsCreator::process_entry(const std::string& name, const std::string& ren
             return false;
         }
     }
+
+    std::vector<std::string> ren_template;
+    const bool have_ren_template = load_template(render_template.c_str(), ren_template);
 
     for (std::vector<std::string>::const_iterator i = entries_.begin(); i != entries_.end(); ++i)
     {
@@ -131,10 +135,36 @@ bool S8AvsCreator::process_entry(const std::string& name, const std::string& ren
 
         if (render.is_open())
         {
-            if (is_vdscript)
-                render << "\"D:\\Program Files\\VirtualDub\\vdub64.exe\" /s " << dir << of_name << std::endl;
+            if (have_ren_template)
+            {
+                const std::string full_filename_c(dir_ + "\\" + clip + ns);
+                const std::string full_filename_d(dir_ + "\\" + clip + ns);
+                const std::string filename_c(full_filename_c + ".avs");
+                const std::string filename_d(full_filename_d + ".avs");
+
+                for (std::vector<std::string>::const_iterator i = ren_template.begin(); i != ren_template.end(); ++i)
+                {
+                    std::string s = *i;
+                    replace_word(s, "$$$dir$$$", dir);
+                    replace_word(s, "$$$dir_c$$$", dir);
+                    replace_word(s, "$$$dir_d$$$", dir);
+                    replace_word(s, "$$$dir_ds$$$", double_backslash_dir);
+                    replace_word(s, "$$$clip$$$", full_filename_d);
+                    replace_word(s, "$$$clip_c$$$", full_filename_c);
+                    replace_word(s, "$$$clip_d$$$", full_filename_d);
+                    replace_word(s, "$$$clip_name$$$", of_name);
+
+                    render << s << std::endl;
+                }
+                render << std::endl;
+            }
             else
-                render << "call render-step.bat %1 " << clip << std::endl;
+            {
+                if (is_vdscript)
+                    render << "\"D:\\Program Files\\VirtualDub\\vdub64.exe\" /s " << dir << of_name << std::endl;
+                else
+                    render << "call render-step.bat %1 " << clip << std::endl;
+            }
         }
 
     }
@@ -158,7 +188,7 @@ void S8AvsCreator::process_entries(const tstring& template_name)
 
     if (!template_name.empty())
     {
-        process_entry(string_utils::to_ansi_string(template_name.c_str(), CP_UTF8), "", "");
+        process_entry(string_utils::to_ansi_string(template_name.c_str(), CP_UTF8), "", "", "");
         return;
     }
 
@@ -170,11 +200,11 @@ void S8AvsCreator::process_entries(const tstring& template_name)
 
         const std::string avs_template_name = dir_ + "\\" + "template" + ns + ".avs";
         const std::string vd_template_name = dir_ + "\\" + "template" + ns + ".vdscript";
-//        const std::string ren_template_name = dir_ + "\\" + "template" + ns + ".render";
+        const std::string ren_template_name = dir_ + "\\" + "template" + ns + ".render";
         const std::string render_bat_name = dir_ + "\\" + "render" + ns + ".bat";
 
-        const bool avs = process_entry(avs_template_name, render_bat_name, ns);
-        const bool vd = process_entry(vd_template_name, render_bat_name, ns);
+        const bool avs = process_entry(avs_template_name, ren_template_name, render_bat_name, ns);
+        const bool vd = process_entry(vd_template_name, ren_template_name, render_bat_name, ns);
 
         if (!avs && !vd)
             break;
@@ -190,4 +220,36 @@ void S8AvsCreator::replace_word(std::string& str, const std::string& word, const
             break;
         str = str.substr(0, pos) + word_new + str.substr(pos + word.size());
     }
+}
+
+bool S8AvsCreator::load_template(const char* name, std::vector<std::string>& templ)
+{
+    tstring tname = string_utils::to_tstring(name, CP_UTF8);
+
+    struct _stat sb;
+    if (_tstat(tname.c_str(), &sb) != 0)
+    {
+//        std::cout << "Couldn't stat \"" << name << "\"" << std::endl;
+        return false;
+    }
+
+    std::ifstream tmpl(tname);
+    if (!tmpl.is_open())
+    {
+        std::cout << "Unable to open file " << name << std::endl;
+        return false;
+    }
+
+    std::string line;
+    while (tmpl.good())
+    {
+        std::getline(tmpl, line);
+        templ.push_back(line);
+    }
+    tmpl.close();
+
+    while (templ.back().empty())
+        templ.pop_back();
+
+    return true;
 }
--- prepare_avs/s8-avs-creator.h
+++ prepare_avs/s8-avs-creator.h
@@ -20,7 +20,8 @@ private:
     std::istream& entries_file_;
     std::vector<std::string> entries_;
 
-    bool process_entry(const std::string& name, const std::string& render_file, const std::string& ns);
+    bool process_entry(const std::string& name, const std::string& render_template, const std::string& render_file, const std::string& ns);
+    bool load_template(const char* name, std::vector<std::string>& templ);
     void replace_word(std::string& str, const std::string& word, const std::string& word_new);
 };
 
