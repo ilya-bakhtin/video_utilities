9c56d5d25e18a1a340d3ac013ff80939fd94352b
Ilya Bakhtin <ilya.bakhtin@gmail.com>
2023-01-24 19:52:18 +0100

    MaskUpdate - lesser recursive solution

--- avisynth_plugins/DropEvery12th/MaskUpdate.cpp
+++ avisynth_plugins/DropEvery12th/MaskUpdate.cpp
@@ -6,6 +6,12 @@
 
 #include <stdio.h>
 
+struct WaveEntry
+{
+    int x_;
+    int y_;
+};
+
 class MaskUpdate : public GenericVideoFilter
 {
 public:
@@ -15,28 +21,178 @@ public:
     PVideoFrame __stdcall GetFrame(int n, IScriptEnvironment* env);
 
 private:
-    int x_limit;
-    int y_limit;
+    int x_limit_;
+    int y_limit_;
 
     mutable std::vector<int> vizited_;
     mutable std::vector<std::pair<int, int> > vizited_p2_;
 
+    mutable std::vector<bool> vizited_w_;
+    mutable std::vector<size_t> vizited_p2_w_;
+
     bool analyze_neighbours(int x, int y, PVideoFrame& src, int level) const;
     bool analyze_neighbours_p2(int x, int y, PVideoFrame& src, int level, int& min_x, int& max_y, int dir, FILE* f);
     void minmax_to_neighbours(int x, int y, PVideoFrame& src, int min_x, int max_y);
+
+    bool analyze_neighbours_wave(PVideoFrame& src, std::vector<WaveEntry>& wave, bool is_black /* area is already detected as black */) const;
+    size_t analyze_neighbours_p2_wave(PVideoFrame& src, std::vector<WaveEntry>& wave, size_t prev_size) const;
 };
 
 MaskUpdate::MaskUpdate(PClip _child, int mask_w, int mask_h, IScriptEnvironment* env) :
     GenericVideoFilter(_child),
-    x_limit(mask_w),
-    y_limit(mask_h)
+    x_limit_(mask_w),
+    y_limit_(mask_h)
 {
-    vizited_.resize(x_limit*y_limit);
-    vizited_p2_.resize(x_limit*y_limit);
+    vizited_.resize(x_limit_*y_limit_);
+    vizited_w_.resize(x_limit_*y_limit_);
+    vizited_p2_.resize(x_limit_*y_limit_);
+    vizited_p2_w_.resize(x_limit_*y_limit_);
 }
 
 MaskUpdate::~MaskUpdate()
 {
+}
+
+struct PointOffset
+{
+    int offs_x_;
+    int offs_y_;
+};
+
+static const PointOffset point_offsets[8] = {
+    { 1,  0},
+    { 1,  1},
+    { 0,  1},
+    {-1, -1},
+    {-1,  0},
+    {-1, -1},
+    { 0, -1},
+    { 1, -1},
+};
+
+bool MaskUpdate::analyze_neighbours_wave(PVideoFrame& dst, std::vector<WaveEntry>& wave, bool is_black /* area is already detected as black */) const
+{
+    if (!vi.IsRGB32())
+        return false;
+
+    const int height = dst->GetHeight();
+    std::vector<WaveEntry> w;
+
+    for (std::vector<WaveEntry>::iterator iw = wave.begin(); iw != wave.end(); ++iw)
+    {
+        const int cx = iw->x_;
+        const int cy = iw->y_;
+
+        for (size_t i = 0; i < sizeof(point_offsets)/sizeof(point_offsets[0]); ++i)
+        {
+            const int x = cx + point_offsets[i].offs_x_;
+            const int y = cy + point_offsets[i].offs_y_;
+
+            if (x < 0 || y >= height)
+                continue; // left or top border. implicitly white.
+
+            if (x >= x_limit_ || y < height - y_limit_) // right or bottom border. implicitly black. if we get here then whole area is black.
+            {
+                is_black = true;
+                continue;
+            }
+
+            const int block_y = y - (height - y_limit_);
+
+            if (!vizited_w_[x + block_y*x_limit_])
+            {
+/*
+FILE* f = fopen("C:\\Users\\ilx\\wrk\\video\\Super8\\log.txt", "a");
+if (f != nullptr)
+{
+    fprintf(f, "* %d, %d (%d)\n", x, block_y, y);
+    fflush(f); fclose(f);
+}
+*/
+                vizited_w_[x + block_y*x_limit_] = true;
+
+                const unsigned char* const dstp = dst->GetReadPtr() + dst->GetPitch() * y;
+                if (dstp[x*4 + 0] == 0 && dstp[x*4 + 1] == 0 && dstp[x*4 + 2] == 0)
+                {
+                    WaveEntry e = {x, y};
+                    w.push_back(e);
+                }
+            }
+        }
+    }
+
+    const bool res = w.empty() ? !is_black : analyze_neighbours_wave(dst, w, is_black);
+
+    if (res)
+    {
+        for (std::vector<WaveEntry>::iterator iw = wave.begin(); iw != wave.end(); ++iw)
+        {
+            const int x = iw->x_;
+            const int y = iw->y_;
+
+            unsigned char* const dstp = dst->GetWritePtr() + dst->GetPitch() * y;
+            dstp[x*4 + 0] = 255;
+            dstp[x*4 + 1] = 255;
+            dstp[x*4 + 2] = 255;
+            dstp[x*4 + 3] = 255;
+        }
+    }
+
+    return res;
+}
+
+size_t MaskUpdate::analyze_neighbours_p2_wave(PVideoFrame& dst, std::vector<WaveEntry>& wave, size_t prev_size) const
+{
+    if (!vi.IsRGB32())
+        return 0;
+
+    if (wave.empty())
+        return prev_size;
+
+    const int height = dst->GetHeight();
+    std::vector<WaveEntry> w;
+
+    for (std::vector<WaveEntry>::iterator iw = wave.begin(); iw != wave.end(); ++iw)
+    {
+        const int cx = iw->x_;
+        const int cy = iw->y_;
+
+        for (size_t i = 0; i < sizeof(point_offsets)/sizeof(point_offsets[0]); ++i)
+        {
+            const int x = cx + point_offsets[i].offs_x_;
+            const int y = cy + point_offsets[i].offs_y_;
+
+            if (x < 0 || y >= height || x >= x_limit_ || y < height - y_limit_) // border is reached.
+                continue;
+
+            const int block_y = y - (height - y_limit_);
+
+            if (vizited_p2_w_[x + block_y*x_limit_] == 0)
+            {
+                vizited_p2_w_[x + block_y*x_limit_] = 1;
+
+                const unsigned char* const dstp = dst->GetReadPtr() + dst->GetPitch() * y;
+                if (dstp[x*4 + 0] != 0 || dstp[x*4 + 1] != 0 || dstp[x*4 + 2] != 0)
+                {
+                    WaveEntry e = {x, y};
+                    w.push_back(e);
+                }
+            }
+        }
+    }
+
+    const size_t res = analyze_neighbours_p2_wave(dst, w, prev_size + w.size());
+
+    for (std::vector<WaveEntry>::iterator iw = wave.begin(); iw != wave.end(); ++iw)
+    {
+        const int x = iw->x_;
+        const int y = iw->y_;
+        const int block_y = y - (height - y_limit_);
+
+        vizited_p2_w_[x + block_y*x_limit_] = res + 1;
+    }
+
+    return res;
 }
 
 bool MaskUpdate::analyze_neighbours(int x, int y, PVideoFrame& src, int level) const
@@ -59,14 +215,14 @@ if (f != nullptr)
         return true;
     }
 
-    if (x >= x_limit || y < height - y_limit)
+    if (x >= x_limit_ || y < height - y_limit_)
     {
 //        fprintf(f, ", false\n");
 //        fclose(f);
         return false;
     }
 
-    const int v = vizited_[x + (y - (height - y_limit))*x_limit];
+    const int v = vizited_[x + (y - (height - y_limit_))*x_limit_];
     if (v != -1)
     {
 //        fprintf(f, ", vizited true\n");
@@ -79,11 +235,11 @@ if (f != nullptr)
     {
 //        fprintf(f, ", white true\n");
 //        fclose(f);
-        vizited_[x + (y - (height - y_limit))*x_limit] = true;
+        vizited_[x + (y - (height - y_limit_))*x_limit_] = true;
         return true;
     }
 
-    vizited_[x + (y - (height - y_limit))*x_limit] = true;
+    vizited_[x + (y - (height - y_limit_))*x_limit_] = true;
 
 //    fprintf(f, ", deeper");
 
@@ -96,7 +252,7 @@ if (f != nullptr)
                      analyze_neighbours(x, y - 1, src, level + 1) &&
                      analyze_neighbours(x + 1, y - 1, src, level + 1);
 
-    vizited_[x + (y - (height - y_limit))*x_limit] = res;
+    vizited_[x + (y - (height - y_limit_))*x_limit_] = res;
 
 //    fprintf(f, ", result %d\n", res);
 //    fclose(f);
@@ -111,14 +267,14 @@ void MaskUpdate::minmax_to_neighbours(int x, int y, PVideoFrame& src, int min_x,
     if (x < 0 || y < 0 || y >= height || x >= src->GetRowSize()/4)
         return;
 
-    if (x >= x_limit || y < height - y_limit)
+    if (x >= x_limit_ || y < height - y_limit_)
         return;
 
     const unsigned char* const srcp = src->GetReadPtr() + src->GetPitch() * y;
     if (srcp[x*4 + 0] == 0 && srcp[x*4 + 1] == 0 && srcp[x*4 + 2] == 0)
         return;
 
-    const size_t viz_idx = x + (y - (height - y_limit))*x_limit;
+    const size_t viz_idx = x + (y - (height - y_limit_))*x_limit_;
     int& viz_mx = vizited_p2_[viz_idx].first;
     int& viz_my = vizited_p2_[viz_idx].second;
 
@@ -148,7 +304,7 @@ bool MaskUpdate::analyze_neighbours_p2(int x, int y, PVideoFrame& src, int level
         return true;
     }
 
-    if (x >= x_limit || y < height - y_limit)
+    if (x >= x_limit_ || y < height - y_limit_)
     {
         if (f != nullptr)
         {
@@ -169,7 +325,7 @@ bool MaskUpdate::analyze_neighbours_p2(int x, int y, PVideoFrame& src, int level
         return true;
     }
 
-    const size_t viz_idx = x + (y - (height - y_limit))*x_limit;
+    const size_t viz_idx = x + (y - (height - y_limit_))*x_limit_;
     int& viz_mx = vizited_p2_[viz_idx].first;
     int& viz_my = vizited_p2_[viz_idx].second;
 
@@ -313,27 +469,42 @@ PVideoFrame __stdcall MaskUpdate::GetFrame(int n, IScriptEnvironment* env)
     if (vi.IsRGB32())
     {
         memcpy(dstp, srcp, src_height*src_pitch);
-        if (dst_width/4 < x_limit || dst_height < y_limit)
+        if (dst_width/4 < x_limit_ || dst_height < y_limit_)
             return dst;
 
-        srcp += (src_height - y_limit)*src_pitch;
-        dstp += (dst_height - y_limit)*dst_pitch;
+        srcp += (src_height - y_limit_)*src_pitch;
+        dstp += (dst_height - y_limit_)*dst_pitch;
 
         for (size_t i = 0; i < vizited_.size(); ++i)
             vizited_[i] = -1;
 
-        for (int y = dst_height - y_limit; y < dst_height; ++y)
+        unsigned char* const dstp_saved = dstp;
+        for (int y = dst_height - y_limit_; y < dst_height; ++y)
         {
-            for (int x = 0; x < x_limit; ++x)
+            for (int x = 0; x < x_limit_; ++x)
             {
-/*
-FILE* f = fopen("D:\\Users\\ilx\\wrk\\video\\Super8\\1983\\1983_720_133\\log.txt", "a");
-if (f != nullptr)
-{
-    fprintf(f, "************ %d, %d\n", x, y);
-    fclose(f);
-}
-*/
+                const bool black = dstp[x*4 + 0] == 0 && dstp[x*4 + 1] == 0 && dstp[x*4 + 2] == 0;
+                if (!black)
+                {
+                    dstp[x*4 + 0] = 255;
+                    dstp[x*4 + 1] = 255;
+                    dstp[x*4 + 2] = 255;
+                    dstp[x*4 + 3] = 255;
+                }
+
+                const int block_y = y - (dst_height - y_limit_);
+                vizited_w_[x + block_y*x_limit_] = !black;
+            }
+            dstp += dst_pitch; // Add the pitch to the destination pointer.
+        }
+
+        dstp = dstp_saved;
+
+        for (int y = dst_height - y_limit_; y < dst_height; ++y)
+        {
+            for (int x = 0; x < x_limit_; ++x)
+            {
+#if 0
                 if (analyze_neighbours(x, y, src, 0))
                 {
                     dstp[x*4 + 0] = 255;
@@ -341,11 +512,38 @@ if (f != nullptr)
                     dstp[x*4 + 2] = 255;
                     dstp[x*4 + 3] = 255;
                 }
+#else
+                const int block_y = y - (dst_height - y_limit_);
+                if (!vizited_w_[x + block_y*x_limit_])
+                {
+/*
+FILE* f = fopen("C:\\Users\\ilx\\wrk\\video\\Super8\\log.txt", "a");
+if (f != nullptr)
+{
+    fprintf(f, "************ %d, %d (%d) - \n", x, block_y, y);
+    fflush(f); fclose(f);
+}
+*/
+                    WaveEntry e = {x, y};
+                    std::vector<WaveEntry> w;
+                    w.push_back(e);
+                    const bool res = analyze_neighbours_wave(dst, w, false);
+/*
+f = fopen("C:\\Users\\ilx\\wrk\\video\\Super8\\log.txt", "a");
+if (f != nullptr)
+{
+    fprintf(f, "%s\n", (res ? "white" : "black"));
+    fflush(f); fclose(f);
+}
+*/
+                }
+#endif
             }
             srcp += src_pitch; // Add the pitch (note use of pitch and not width) of one line (in bytes) to the source pointer
             dstp += dst_pitch; // Add the pitch to the destination pointer.
         }
 
+#if 0
         srcp = srcp_org + (src_height - 1)*src_pitch;
         dstp = dstp_org + (dst_height - 1)*dst_pitch;
 
@@ -356,9 +554,9 @@ if (f != nullptr)
         }
 
         FILE* f = nullptr; //fopen("D:\\Users\\ilx\\wrk\\video\\Super8\\1983\\1983_720_133\\log.txt", "a");
-        for (int y = dst_height - 1; y >= dst_height - y_limit; --y)
+        for (int y = dst_height - 1; y >= dst_height - y_limit_; --y)
         {
-            for (int x = 0; x < x_limit; ++x)
+            for (int x = 0; x < x_limit_; ++x)
             {
                 if (f != nullptr)
                 {
@@ -387,7 +585,61 @@ if (f != nullptr)
         }
         if (f != nullptr)
             fclose(f);
+#else
+        dstp = dstp_saved;
 
+        for (int y = dst_height - y_limit_; y < dst_height; ++y)
+        {
+            for (int x = 0; x < x_limit_; ++x)
+            {
+                const int block_y = y - (dst_height - y_limit_);
+                vizited_p2_w_[x + block_y*x_limit_] =
+                    dstp[x*4 + 0] == 0 && dstp[x*4 + 1] == 0 && dstp[x*4 + 2] == 0 ? 1 : 0;
+            }
+            dstp += dst_pitch; // Add the pitch to the destination pointer.
+        }
+
+        dstp = dstp_saved;
+
+        size_t max_res = 0;
+
+        for (int y = dst_height - y_limit_; y < dst_height; ++y)
+        {
+            for (int x = 0; x < x_limit_; ++x)
+            {
+                const int block_y = y - (dst_height - y_limit_);
+                if (vizited_p2_w_[x + block_y*x_limit_] == 0)
+                {
+                    WaveEntry e = {x, y};
+                    std::vector<WaveEntry> w;
+                    w.push_back(e);
+                    const size_t res = analyze_neighbours_p2_wave(dst, w, 1);
+                    if (max_res < res)
+                        max_res = res;
+                }
+            }
+            dstp += dst_pitch; // Add the pitch to the destination pointer.
+        }
+
+        dstp = dstp_saved;
+
+        for (int y = dst_height - y_limit_; y < dst_height; ++y)
+        {
+            for (int x = 0; x < x_limit_; ++x)
+            {
+                const int block_y = y - (dst_height - y_limit_);
+                const size_t val = vizited_p2_w_[x + block_y*x_limit_];
+                if (val != 1 && val != max_res + 1)
+                {
+                    dstp[x*4 + 0] = 0;
+                    dstp[x*4 + 1] = 0;
+                    dstp[x*4 + 2] = 0;
+                    dstp[x*4 + 3] = 255;
+                }
+            }
+            dstp += dst_pitch; // Add the pitch to the destination pointer.
+        }
+#endif
     }
 
     return dst;
